1bf1e9a4d0c09ee33b181568fc8d3d7204fa5465
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-03-29 12:38:48 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,69 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import requests
+import time
+st.set_page_config(layout="wide")
+st.title("Penny Stock Data Science")
+polygon_key = st.secrets["polygon_key"]
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def fetch_data():
+    master_list = []
+    url = "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?include_otc=true&apiKey="+polygon_key
+    #add something to get lists of otc vs listed
+    response = requests.get(url)
+    data = response.json()
+    tickers_list = data["tickers"]
+    for i in tickers_list:
+        ticker = i["ticker"]
+        percentage_str = i["todaysChangePerc"]
+        percentage_float = float(percentage_str)
+        change_str = i["todaysChange"]
+        change_float = float(change_str)
+        dayDic = i["day"]
+        dayV = dayDic["v"]
+        dayVint = int(dayV)
+        dayVW = dayDic["vw"]
+        dayVWfloat = float(dayVW)
+        daylastTrade = i["lastTrade"]
+        dayPrice = daylastTrade["p"]
+        dayPriceFloat = float(dayPrice)
+        dollarValue = int(dayVint*dayVWfloat)
+        epoch_time = int(time.time())
+        new_list = [ticker,dayPriceFloat,dayVWfloat,percentage_float,dayVint,dollarValue,epoch_time]
+        master_list.append(new_list)
+    columns = ["Ticker","Price","VWAP","% Change","Volume","$ Volume","Time"]
+    df = pd.DataFrame(master_list, columns=columns)
+    df=df[df["$ Volume"] > 5000]
+    df_sorted = df[df['Price'] > 1].sort_values(by="% Change", ascending=False).head(100)
+    df_sorted['Price'] = df_sorted['Price'].round(2)
+    df_sorted['VWAP'] = df_sorted['VWAP'].round(2)
+    df_sorted["% Change"] = df_sorted["% Change"].round(2)
+    df_sorted["% Change"] = df_sorted["% Change"].apply(lambda x: '{:+}%'.format(x) if x >= 0 else '{:-}%'.format(x))
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    return df_sorted
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def main():
+    st.header('Stocks')
+    df1 = st.empty()
+    
+    
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    # Infinite loop to continuously update data
+    while True:
+        try:
+            # Fetch data from Polygon.io API
+            new_df1 = fetch_data()
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+            df1.dataframe(new_df1, hide_index=True)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+            # Sleep for 1 second before making the next API call
+            time.sleep(.1)
+
+        except Exception as e:
+            continue
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    main()
