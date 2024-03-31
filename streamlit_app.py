de0243c8e3959a5ce63579243fbe3b81d9cfc4a0
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-03-31 15:59:28 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ polygon_key = st.secrets["polygon_key"]
 
 
 def fetch_data():
+    url_naz = "http://www.nasdaqtrader.com/dynamic/SymDir/nasdaqtraded.txt"
+    df_naz = pd.read_csv(url_naz, delimiter="|")
+    nasdaq_symbol_list = df_naz['Symbol'].dropna().tolist()
     master_list = []
     url = "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?include_otc=true&apiKey="+polygon_key
     #add something to get lists of otc vs listed
@@ -30,27 +33,30 @@ def fetch_data():
         dayPriceFloat = float(dayPrice)
         dollarValue = int(dayVint*dayVWfloat)
         epoch_time = int(time.time())
-        new_list = [ticker,dayPriceFloat,dayVWfloat,percentage_float,dayVint,dollarValue,epoch_time]
+        if ticker in nasdaq_symbol_list:
+            loc = "listed"
+        else:
+            loc = "otc"
+        new_list = [ticker,dayPriceFloat,dayVWfloat,percentage_float,dayVint,dollarValue,loc,epoch_time]
         master_list.append(new_list)
-    columns = ["Ticker","Price","VWAP","% Change","Volume","$ Volume","Time"]
+    columns = ["Ticker","Price","VWAP","% Change","Volume","$ Volume","Venue","Time"]
     df = pd.DataFrame(master_list, columns=columns)
     df=df[df["$ Volume"] > 5000]
-    df_sorted = df[df['Price'] > 1].sort_values(by="% Change", ascending=False).head(100)
+    df_sorted = df.sort_values(by="% Change", ascending=False)
     df_sorted['Price'] = df_sorted['Price'].round(2)
     df_sorted['VWAP'] = df_sorted['VWAP'].round(2)
     df_sorted["% Change"] = df_sorted["% Change"].round(2)
     df_sorted["% Change"] = df_sorted["% Change"].apply(lambda x: '{:+}%'.format(x) if x >= 0 else '{:-}%'.format(x))
-    trip_otc_df = df_sorted[df_sorted['Price'] < 0.001]
-    url3 = "http://www.nasdaqtrader.com/dynamic/SymDir/nasdaqtraded.txt"
-    df27 = pd.read_csv(url3, delimiter="|")
+    naz_df = df_sorted[df_sorted['Venue'] == 'listed']
+    naz_df = naz_df[naz_df['Price'] < 25]
+    otc_df = df_sorted[df_sorted['Venue'] == 'otc']
+    trip_otc_df = otc_df[otc_df['Price'] < 0.001]
+    sub_otc_df = otc_df[(otc_df['Price'] >= 0.001) & (otc_df['Price'] <= 0.01)]
+    penny_plus_df = otc_df[otc_df['Price'] > 0.01]
 
-    return df_sorted, trip_otc_df, df27
+    return naz_df, trip_otc_df, sub_otc_df, penny_plus_df
 
 def main():
-    #st.header('Stocks')
-    #df1 = st.empty()
-    #st.header('Sub Penny')
-    #df3 = st.empty()
     columns = st.columns(4)
     with columns[0]:
         st.header('Stocks')
@@ -64,19 +70,16 @@ def main():
     with columns[3]:
         st.header('Penny +')
         df4 = st.empty()
-    
 
     # Infinite loop to continuously update data
     while True:
         try:
             # Fetch data from Polygon.io API
-            new_df1 , new_df2, new_df3= fetch_data()
-
-
+            new_df1, new_df2, new_df3, new_df4 = fetch_data()
             df1.dataframe(new_df1, hide_index=True)
             df2.dataframe(new_df2, hide_index=True)
             df3.dataframe(new_df3, hide_index=True)
-
+            df4.dataframe(new_df4, hide_index=True)
             # Sleep for 1 second before making the next API call
             time.sleep(.1)
 
